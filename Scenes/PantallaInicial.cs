4975dd240fb69f0afdd56b5b5437fe5ac043f162
IgnacioMojsa <ignaciomojsa02@gmail.com>
2026-08-25 20:13:50 -0300

    Cambio de GDScript por C# en codigo de Pantalla Inicial

---
+++ Scenes/PantallaInicial.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public partial class PantallaInicial : Control
+{
+	private void _on_play_button_pressed(){
+		GD.Print("El botón play ha sido presionado");
+        GetTree().ChangeSceneToFile("res://Scenes/escenaPrueba.tscn");
+	}
+	
+	private void _on_exit_button_pressed(){
+		GetTree().Quit();
+	}
+};
