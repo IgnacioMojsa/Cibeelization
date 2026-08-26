fa57f8f11b78cbd1dff88eb0461358baace8cff4
p0chyly <maitena.pochyly@estudiantes.unahur.edu.ar>
2026-08-26 02:42:25 -0300

    Clases Abeja y Abeja Reina solo con HP

---
+++ Scripts/AbejaReina.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public partial class AbejaReina : Abeja
+{
+	public override void _Ready()
+	{
+		GD.Print("Abeja reina creada");
+	}
+	
+	public override void _Process(double delta)
+	{
+	}
+}
--- Scripts/PantallaInicial.cs
+++ Scripts/PantallaInicial.cs
@@ -5,7 +5,7 @@ public partial class PantallaInicial : Control
 {
 	private void _on_play_button_pressed(){
 		GD.Print("El botón play ha sido presionado");
-        GetTree().ChangeSceneToFile("res://Scenes/escenaPrueba.tscn");
+		GetTree().ChangeSceneToFile("res://Scenes/escenaPrueba.tscn");
 	}
 	
 	private void _on_exit_button_pressed(){
