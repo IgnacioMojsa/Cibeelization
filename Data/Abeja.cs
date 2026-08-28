ca6fe3bbbc30696aa727bd4c022c015f705e33a8
p0chyly <maitena.pochyly@estudiantes.unahur.edu.ar>
2026-08-27 22:53:43 -0300

    void de input incompleto

--- Data/Abeja.cs
+++ Data/Abeja.cs
@@ -4,4 +4,12 @@ using System;
 public partial class Abeja : Node3D{
 	public int HP { get; set; } = 5;
 
+	public override void _Input(InputEvent @event){
+		if (@event is not InputEventMouseButton mouse ||
+            mouse.ButtonIndex != MouseButton.Left ||
+            !mouse.Pressed)
+            return;
+			GD.Print("Hiciste click");
+	}
+
 }
