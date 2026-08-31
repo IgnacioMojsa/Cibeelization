da8ce5c20a9ec5cacb10973b0f00b5367693a8e3
IgnacioMojsa <ignaciomojsa02@gmail.com>
2026-08-31 11:00:54 -0300

    Seleccion de jugadores para el desarrollo de la partida

--- Visual/GameUI.cs
+++ Visual/GameUI.cs
@@ -4,6 +4,7 @@ using System;
 public partial class GameUI : Control
 {
 	private Label resultadoDados;
+	private int cantidadJugadores {get; set;}
 
 	public override void _Ready(){
 		resultadoDados = GetNode<Label>("HBoxContainer/NumeroDado/MarginContainer/Label");
@@ -12,14 +13,46 @@ public partial class GameUI : Control
 	private void Jugar(){
 		PanelContainer UIComienzo = GetNode<PanelContainer>("MenuComienzo");
 
+		GD.Print("El botón play ha sido presionado");
+
 		UIComienzo.Visible = true;
 	}
 
 	private void ComenzarPartida(){
-		GD.Print("El botón play ha sido presionado");
+		cantidadJugadores = ObtenerCantJugadores();
+		
+		GD.Print("La partida se desarrollara con " + cantidadJugadores + " jugadores");
 		GetTree().ChangeSceneToFile("res://Scenes/escenaPrueba.tscn");
 	}
 	
+	private void SeleccionarCantidadDeJugadores(bool estaPresionado){
+		if (!estaPresionado) return;
+		
+		if (estaPresionado){
+        	cantidadJugadores = ObtenerCantJugadores();
+        	GD.Print($"Cantidad de jugadores seleccionada: {cantidadJugadores}");
+        }
+	}
+
+	private int ObtenerCantJugadores(){
+		var check2 = GetNode<CheckBox>("MenuComienzo/MarginContainer/VBoxContainer/VBoxContainer/2Players/CheckBox");
+    	var check3 = GetNode<CheckBox>("MenuComienzo/MarginContainer/VBoxContainer/VBoxContainer/3Players/CheckBox");
+		var check4 = GetNode<CheckBox>("MenuComienzo/MarginContainer/VBoxContainer/VBoxContainer/4Players/CheckBox");
+		
+		if(check2 != null && check2.ButtonPressed){
+			return 2;
+		}
+		else if(check3 != null && check3.ButtonPressed){
+			return 3;
+		}
+		else if(check4 != null && check4.ButtonPressed){
+			return 4;
+		}
+		else{
+			return 2;
+		}
+	}
+
 	private void SalirDelJuego(){
 		GetTree().Quit();
 	}
