652a6f9fbf4a530af3b01c24d58a85ed9e8e7e74
mmonescao <mmonescao@gmail.com>
2026-08-31 22:51:56 -0300

    Agregado collision shape al hexagon tile

--- Visual/PlayerManager.cs
+++ Visual/PlayerManager.cs
@@ -1,79 +1,102 @@
 using Godot;
 using System.Collections.Generic;
 
-public partial class PlayerManager: Node3D{
+public partial class PlayerManager : Node3D
+{
+	[Export] private Camera3D camera;
 
-    [Export] private Camera3D camera;
+	private List<Node3D> VisualesJugadores = new List<Node3D>();
+	private Vector3 posicionNueva; 
+	private Node3D ReinaDelJugador; 
 
-    private List<Node3D> VisualesJugadores = new List<Node3D>();
+	public override void _Ready()
+	{
+		InstanciarJugadores();
+		EstablecerSpawns();
+	}
 
-    private Vector3 posicionNueva; 
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if (@event.IsActionPressed("move"))
+		{
+			if (GetViewport().GuiGetHoveredControl() != null)
+				return;
 
-    private Node3D ReinaDelJugador; 
+			if (camera == null)
+				camera = GetViewport().GetCamera3D();
 
-    public override void _Ready(){
-        InstanciarJugadores();
-        EstablecerSpawns();
-    }
+			// Solo movemos si el raycast encontró una posición válida
+			if (ObtenerPosicionNueva())
+			{
+				MoverAbeja();
+			}
+		}
+	}
 
-    public override void _UnhandledInput(InputEvent @event){
-        if (@event.IsActionPressed("move"))
-        {
-            if(GetViewport().GuiGetHoveredControl() != null)
-            return;
+	public bool ObtenerPosicionNueva()
+	{
+		Vector2 mousePosition = GetViewport().GetMousePosition();
+		Vector3 rayOrigin = camera.ProjectRayOrigin(mousePosition);
+		Vector3 rayEnd = rayOrigin + camera.ProjectRayNormal(mousePosition) * 1000.0f;
 
-            if (camera == null)
-                camera = GetViewport().GetCamera3D();
+		var spaceState = GetWorld3D().DirectSpaceState;
+		var query = PhysicsRayQueryParameters3D.Create(rayOrigin, rayEnd);
 
-            ObtenerPosicionNueva();
-            MoverAbeja();
-        }
-    }
+		var result = spaceState.IntersectRay(query);
 
-    public void ObtenerPosicionNueva(){
-        Vector2 mousePosition = GetViewport().GetMousePosition();
-        Vector3 rayOrigin = camera.ProjectRayOrigin(mousePosition);
-        Vector3 rayEnd = rayOrigin + camera.ProjectRayNormal(mousePosition) * 1000.0f;
+		if (result.Count > 0)
+		{
+			// Casteo directo desde Godot.Variant a Vector3
+			posicionNueva = result["position"].AsVector3();
+			GD.Print("Nueva posición guardada: " + posicionNueva);
+			return true;
+		}
 
-        var spaceState = GetWorld3D().DirectSpaceState;
-        var query = PhysicsRayQueryParameters3D.Create(rayOrigin, rayEnd);
+		GD.Print("El raycast no chocó con ningún objeto 3D con colisión.");
+		return false;
+	}
 
-        var result = spaceState.IntersectRay(query);
+	public void MoverAbeja()
+	{
+		if (VisualesJugadores.Count > 0)
+		{
+			ReinaDelJugador = VisualesJugadores[0]; 
 
-        if (result.Count > 0){
-            posicionNueva = (Vector3)result["position"];
-            GD.Print("Nueva posicion guardada" + posicionNueva);
-        }
-    }
+			if (ReinaDelJugador != null)
+			{
+				ReinaDelJugador.GlobalPosition = posicionNueva;
+			}
+		}
+	}
 
-    public void MoverAbeja(){
-        ReinaDelJugador = VisualesJugadores[0]; 
+	public void InstanciarJugadores()
+	{
+		var VisualReina1 = GD.Load<PackedScene>("res://Scenes/AbejaReina.tscn");
+		
+		for (int j = 0; j < GameManager.Instance.cantidadJugadores; j++)
+		{
+			var InstanciaNueva = VisualReina1.Instantiate<Node3D>();
+			AddChild(InstanciaNueva);
+			VisualesJugadores.Add(InstanciaNueva);
+		}
+	}
 
-        if (ReinaDelJugador != null)
-        {
-            ReinaDelJugador.GlobalPosition = posicionNueva;
-        }
-    }
+	public void EstablecerSpawns()
+	{ 
+		List<Vector3> posiciones = new List<Vector3>() 
+		{
+			new Vector3(-6, 0, 2), 
+			new Vector3(25, 0, 3), 
+			new Vector3(3, 0, 14), 
+			new Vector3(19, 0, 19)
+		};
 
-    /*public void PrecargarJugadores(){
-        var VisualReina1 = GD.Preload("res://Assets/AbejaReina.glb");
-    }*/
-
-    public void InstanciarJugadores(){
-        var VisualReina1 = GD.Load<PackedScene>("res://Scenes/AbejaReina.tscn");
-        
-        for (int j = 0; j < GameManager.Instance.cantidadJugadores; j++){
-            var InstanciaNueva = VisualReina1.Instantiate<Node3D>();
-            AddChild(InstanciaNueva);
-            VisualesJugadores.Add(InstanciaNueva);
-        }
-    }
-
-    public void EstablecerSpawns(){ 
-		List<Vector3> posiciones = new List<Vector3>() {new Vector3(-6, 0, 2), new Vector3(25, 0, 3), new Vector3(3, 0, 14), new Vector3(19, 0, 19)};
-        for (int i = 0; i < VisualesJugadores.Count; i++)
-        {
-            VisualesJugadores[i].GlobalPosition = posiciones[i];
-        }
+		for (int i = 0; i < VisualesJugadores.Count; i++)
+		{
+			if (i < posiciones.Count)
+			{
+				VisualesJugadores[i].GlobalPosition = posiciones[i];
+			}
+		}
 	}
 }
